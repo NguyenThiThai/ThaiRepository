7236e6ffd21771a6c7e012750424de3ab2aaa854
nguyen thi thai <thai-n@evoltech.co.jp>
2022-12-23 10:35:40 +0900

    commit cho testsuite 2

---
+++ Test Suites/Test Suite_TC_committest.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite_TC_committest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>0fb72827-270d-424f-bb4c-082087ad786c</testSuiteGuid>
+   <testCaseLink>
+      <guid>8867a593-8017-4da4-a0f3-7e98ba1fc587</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/committest</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
